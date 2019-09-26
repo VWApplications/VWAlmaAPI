a60ed2390f8b791f83f898a6707e65b94f626584
Victor Deon <victorhad@gmail.com>
2019-09-26 03:16:46 -0300

    Insert queryset filter into core news

--- project/core/views.py
+++ project/core/views.py
@@ -1,7 +1,18 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
 from .serializers import NewsTagsSerializer, TagSerializer
 from .models import News, Tag
 from .permissions import CreateUpdateDestroyAdminPermission
+
+
+class CustomPagination(PageNumberPagination):
+    """
+    Separar a lista de tickets em páginas.
+    """
+
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 
 class NewsViewSet(ModelViewSet):
@@ -12,9 +23,27 @@ class NewsViewSet(ModelViewSet):
     `partial_update()`, `destroy()` and `list()` actions.
     """
 
-    queryset = News.objects.all()
     serializer_class = NewsTagsSerializer
     permission_classes = (CreateUpdateDestroyAdminPermission,)
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        """
+        Make a filter if passed, otherwise return all news.
+        """
+
+        queryset = News.objects.all()
+
+        search = self.request.query_params.get('search', None)
+        tag = self.request.query_params.get('tag', None)
+
+        if search:
+            queryset = queryset.filter(title__icontains=search)
+
+        if tag:
+            queryset = queryset.filter(tags__title__icontains=tag)
+
+        return queryset
 
 
 class TagsViewSet(ModelViewSet):
