9888131033fc6c858c07de87ebd409364beef579
Victor Deon <victorhad@gmail.com>
2019-10-01 03:29:58 -0300

    Insert new mailgun account

--- project/vwa/config/email.py
+++ project/vwa/config/email.py
@@ -2,9 +2,9 @@
 EMAIL_BACKEND_DEV = 'django.core.mail.backends.console.EmailBackend'
 
 ANYMAIL = {
-    "MAILGUN_API_KEY": "7d960b116ec69cb3bd909ba25706041a-f877bd7a-351943a5",
-    "MAILGUN_SENDER_DOMAIN": 'sandbox43d3bc2d2ec44a6688b52d324f1f7cb3.mailgun.org'
+    "MAILGUN_API_KEY": "2a982de42eedd4b4a923a1084a454cf0-af6c0cec-013d6cb9",
+    "MAILGUN_SENDER_DOMAIN": 'sandboxa9413bd50595408390d634130398e610.mailgun.org'
 }
 
 EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
-DEFAULT_FROM_EMAIL = "victorhad@gmail.com"
+DEFAULT_FROM_EMAIL = "vwapplication@gmail.com"
